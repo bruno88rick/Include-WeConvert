70f2fabeb6b32b689065820d9f6fcc0071c20b6f
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2023-11-24 17:06:29 -0300

    Initial Commit

---
+++ Include WeConvert/Include_WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Include_WeConvertApp.swift
+//  Include WeConvert
+//
+//  Created by Bruno Oliveira on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Include_WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
